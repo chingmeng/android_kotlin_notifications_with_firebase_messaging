1c070db7c43941e3c49f57fcbbf8e814d870d1d1
chingmeng <sum2infinity@gmail.com>
2020-09-20 22:44:03 +0800

    Add icon and big picture style to notification

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -52,6 +52,15 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     )
 
     // TODO: Step 2.0 add style
+    val eggImage = BitmapFactory.decodeResource(
+        applicationContext.resources,
+        R.drawable.cooked_egg
+    )
+
+    // Expanded notification style
+    val bigPicStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(eggImage)
+        .bigLargeIcon(null)
 
     // TODO: Step 2.2 add snooze action
 
@@ -74,6 +83,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
             .setAutoCancel(true)
 
         // TODO: Step 2.1 add style to builder
+            .setStyle(bigPicStyle)
+            .setLargeIcon(eggImage)
 
         // TODO: Step 2.3 add snooze action
 
